ffe96e9d9bf879af43fd7c2f52a6ecd103486339
Rishabh Lal <94430943+Rishblol@users.noreply.github.com>
2022-05-21 08:54:28 +0530

    Add files via upload

--- flask_cl/market/routes.py
+++ flask_cl/market/routes.py
@@ -1,3 +1,4 @@
+#IMPORTS
 from market import app
 from flask import Flask, render_template, redirect, url_for, flash, session, request
 from market.models import Item, User
@@ -6,17 +7,44 @@ from market import db
 from flask_login import login_user, logout_user, login_required
 from flask_socketio import SocketIO, join_room, leave_room, emit
 from flask_session import Session
+import time
+import wolframalpha
+import pyttsx3
+import json
+import random
+import speech_recognition as sr
+import datetime
+import webbrowser
+import os
+import pyjokes
+import time
+import requests
+import shutil
+from clint.textui import progress
+from urllib.request import urlopen
+from newsapi import NewsApiClient
+saved_locations = []
+engine = pyttsx3.init('sapi5')# Microsoft speech application
+voices = engine.getProperty('voices')
 
+#HOME -- NANOSOFT
 @app.route('/')
 @app.route('/home')
 def home_page():
     return render_template('home.html')
 
-@app.route('/chat_bot')
+#CHATBOT 
+@app.route('/chat_bot',methods=['GET','POST'])
 @login_required
 def chat_bot():
+    return render_template('index_chatbot.html')
+
+@app.route('/JARVIS',methods=['GET','POST'])
+def jarvis():
     return render_template('chat_bot.html')
 
+
+#REGISTER AND LOGIN 
 @app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
@@ -45,7 +73,7 @@ def login_page():
         ):
             login_user(attempted_user)
             flash(f'Success! You are logged in as: {attempted_user.username}', category='success')
-            return redirect(url_for('chat_bot'))
+            return redirect(url_for('home_page'))
         else:
             flash('Username and password are not match! Please try again', category='danger')
 
@@ -57,8 +85,7 @@ def logout_page():
     flash("You have been logged out!", category='info')
     return redirect(url_for("home_page"))
 
-
-
+#CHATTING ROOM :- NANOCHAT
 socketio = SocketIO(app, manage_session=False)
 
 @socketio.on('join', namespace='/chat')
@@ -67,12 +94,10 @@ def join(message):
     join_room(room)
     emit('status', {'msg':  session.get('username') + ' has entered the room.'}, room=room)
 
-
 @socketio.on('text', namespace='/chat')
 def text(message):
     room = session.get('room')
     emit('message', {'msg': session.get('username') + ' : ' + message['msg']}, room=room)
-
 
 @socketio.on('left', namespace='/chat')
 def left(message):
